3c107b7fc5fe72046647564079433950825143a5
daAftermath <44099426+daGalileo@users.noreply.github.com>
2019-04-10 20:01:10 -0700

    fixed http redirect js

--- scripts/httpsRedirect.js
+++ scripts/httpsRedirect.js
@@ -1,3 +1,25 @@
-if (document.location.protocol != "https:") {
+var devEnv = false;
+
+// TESTING ON IIS SERVER
+if (document.location.host.includes("localhost")) {
+    devEnv = true;
+}
+
+// TESTING ON VS CODE LIVE SERVER
+if (document.location.host.includes("127")) {
+    devEnv = true;
+}
+
+// TESTING ON LOCAL NETWORK
+if (document.location.host.includes("192")) {
+    devEnv = true;
+}
+
+// USING FILE SYSTEM
+if (document.location.protocol != "file:") {
+    devEnv = true;
+}
+
+if (!devEnv && document.location.protocol != "https:") {
     document.location = document.URL.replace(/^http:/i, "https:");
 }
