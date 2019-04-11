1ee6185a05fdcbc0e2504879666aedb0adfbe48b
daAftermath <44099426+daGalileo@users.noreply.github.com>
2019-04-10 19:27:19 -0700

    removed sendmail.js

--- scripts/sendEmail.js
+++
@@ -1,8 +0,0 @@
-$('#contactForm').submit(function (e) {
-    $.ajax({
-        url: "https://formspree.io/jassbhamra@outlook.com",
-        method: "POST",
-        data: $(this).serialize(),
-        dataType: "json"
-    });
-})
